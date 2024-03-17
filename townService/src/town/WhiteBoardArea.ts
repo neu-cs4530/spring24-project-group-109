b2ceae0d07c88440f24ce39278da734b497b3a68
Oceane <kenfacktonfack.o@northeastern.edu>
2024-03-17 18:53:13 -0400

    WhiteBoard Area Funtionality Completed

--- townService/src/town/WhiteBoardArea.ts
+++ townService/src/town/WhiteBoardArea.ts
@@ -1,28 +1,57 @@
-import Player from '../lib/Player';
-import { Interactable, InteractableCommand, InteractableCommandReturnType, InteractableType } from '../types/CoveyTownSocket';
-import InteractableArea from './InteractableArea';
+import WhiteBoardModel from './WhiteBoardModel';
+import {
+  DrawCommand,
+  EraseCommand,
+  InteractableCommand,
+  InteractableCommandReturnType,
+  InteractableType,
+  Player,
+} from '../types/CoveyTownSocket';
+import InvalidParametersError, {
+  INVALID_BOARD_MESSAGE,
+  INVALID_DRAWER_MESSAGE,
+} from '../lib/InvalidParametersError';
 
-
-const WHITEBOARD_HEIGHT = 10;
-const WHITEBOARD_WIDTH= 10;
-
-export default class WhiteBoardArea extends GameArea<PictionaryGame> {  
-    // public constructor() {
-    //     super();
-    // }
-
-}
-
-private _getBoard(): Color[][] {
-    const board: Color[][] = [];
-    for (let i = 0; i < WHITEBOARD_HEIGHT; i += 1) {
-      const row: Color[] = [];
-      for (let j = 0; j < WHITEBOARD_WIDTH; j += 1) {
-            row.push(`#${'FFFFFF'}`);
-      }
-      board.push(row);
-    }
-    return board;
+export default class WhiteBoardArea extends WhiteBoardModel {
+  protected getType(): InteractableType {
+    return 'WhiteBoardArea';
   }
 
+  public handleCommand<CommandType extends InteractableCommand>(
+    command: CommandType,
+    player: Player,
+  ): InteractableCommandReturnType<CommandType> {
+    if (player.id === '0' && this.board) {
+      // ToDo : Add player Drawer check
+      if (command.type === 'DrawCommand') {
+        const drawCommand = command as DrawCommand;
+        const { board } = this;
+        if (!board) {
+          throw new InvalidParametersError(INVALID_BOARD_MESSAGE);
+        }
+        this.draw(drawCommand.drawing);
+        return undefined as InteractableCommandReturnType<CommandType>;
+      }
+      if (command.type === 'EraseCommand') {
+        const drawCommand = command as EraseCommand;
+        const { board } = this;
+        if (!board) {
+          throw new InvalidParametersError(INVALID_BOARD_MESSAGE);
+        }
+        this.erase(drawCommand.drawing);
+        return undefined as InteractableCommandReturnType<CommandType>;
+      }
+      if (command.type === 'ResetCommand') {
+        const { board } = this;
+        if (!board) {
+          throw new InvalidParametersError(INVALID_BOARD_MESSAGE);
+        }
+        this.reset();
+        return undefined as InteractableCommandReturnType<CommandType>;
+      }
 
+      throw new InvalidParametersError(INVALID_DRAWER_MESSAGE);
+    }
+    return undefined as InteractableCommandReturnType<CommandType>;
+  }
+}
