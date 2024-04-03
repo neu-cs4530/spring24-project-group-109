117f2eeadd0b780f41177b01328f3f9b9e3d4aab
ashnajainneu <jain.ashna@northeastern.edu>
2024-04-03 12:20:07 -0400

    file mgmt

--- frontend/src/components/SocialSidebar/PlayerName.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import PlayerController from '../../classes/PlayerController';
-
-type PlayerNameProps = {
-  player: PlayerController;
-};
-export default function PlayerName({ player }: PlayerNameProps): JSX.Element {
-  return <>{player.userName}</>;
-}
